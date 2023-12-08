ae153864bf52279093cf4da8f139587cbf8cc89b
lunarhike <lunarhike@gmail.com>
2023-12-08 20:29:43 +0100

    added invoices fetching

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,3 +1,15 @@
-<p>Dashboard page</p>
+<script>
+	// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
-<slot />
+	export let data;
+
+	console.log(data);
+</script>
+
+<main>
+	<h1 class="font-lusitana mb-4 text-xl md:text-2xl">Dashboard</h1>
+	<div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+	<div class="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+		<!-- <LatestInvoices latestInvoices={data.latestInvoices} /> -->
+	</div>
+</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
+import dotenvExpand from 'dotenv-expand';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: 'svelte',
-			autoInstall: true
-		})
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	// This check is important!
+	if (mode === 'development') {
+		const env = loadEnv(mode, process.cwd(), '');
+		dotenvExpand.expand({ parsed: env });
 	}
+
+	return {
+		plugins: [
+			sveltekit(),
+			Icons({
+				compiler: 'svelte',
+				autoInstall: true
+			})
+		],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
+	};
 });
