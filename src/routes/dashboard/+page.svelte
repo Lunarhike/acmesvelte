8d51ae22ab777d308c5dfce1ca8a42443f6355b5
lunarhike <lunarhike@gmail.com>
2023-12-08 21:40:12 +0100

    added customer photos

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+	import LatestInvoices from './components/LatestInvoices.svelte';
 
 	export let data;
 
@@ -10,6 +10,6 @@
 	<h1 class="font-lusitana mb-4 text-xl md:text-2xl">Dashboard</h1>
 	<div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
 	<div class="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-		<!-- <LatestInvoices latestInvoices={data.latestInvoices} /> -->
+		<LatestInvoices latestInvoices={data.latestInvoices} />
 	</div>
 </main>
---
+++ src/routes/dashboard/components/LatestInvoices.svelte
@@ -0,0 +1,40 @@
+<script>
+	import ArrowPathIcon from '~icons/ph/arrows-clockwise'; // You need to find a Svelte alternative for this
+	export let latestInvoices = [];
+</script>
+
+<div class="flex w-full flex-col md:col-span-4 lg:col-span-4">
+	<h2 class="font-lusitana mb-4 text-xl md:text-2xl">Latest Invoices</h2>
+	<div class="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+		<div class="bg-white px-6">
+			{#each latestInvoices as invoice, i (invoice.id)}
+				<div
+					class={i !== 0
+						? 'flex flex-row items-center justify-between py-4 border-t'
+						: 'flex flex-row items-center justify-between py-4'}
+				>
+					<div class="flex items-center">
+						<img
+							src={invoice.image_url}
+							alt={`${invoice.name}'s profile picture`}
+							class="mr-4 rounded-full"
+							width={32}
+							height={32}
+						/>
+						<div class="min-w-0">
+							<p class="truncate text-sm font-semibold md:text-base">{invoice.name}</p>
+							<p class="hidden text-sm text-gray-500 sm:block">{invoice.email}</p>
+						</div>
+					</div>
+					<p class="font-lusitana truncate text-sm font-medium md:text-base">
+						{invoice.amount}
+					</p>
+				</div>
+			{/each}
+		</div>
+		<div class="flex items-center pb-2 pt-6">
+			<ArrowPathIcon class="h-5 w-5 text-gray-500" />
+			<h3 class="ml-2 text-sm text-gray-500">Updated just now</h3>
+		</div>
+	</div>
+</div>
