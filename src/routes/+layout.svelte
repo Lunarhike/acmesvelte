0880ae4511e347efafed37885dead5b16243c5dd
lunarhike <lunarhike@gmail.com>
2023-12-08 21:13:06 +0100

    added env variables, added web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import '@fontsource/lusitana';
 	import '@fontsource-variable/inter';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
